1577e4929458dbc6ee5d5260cc59392a677e1132
Luigia <funtronik@gmail.com>
2018-08-20 02:36:52 +0200

    ?

--- webapp/controller/LogonPage.controller.js
+++ webapp/controller/LogonPage.controller.js
@@ -1,11 +1,21 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
- ], function (Controller) {
+], function (Controller) {
     "use strict";
     return Controller.extend("app.webapp.controller.LogonPage", {
-        onShowHello : function () {
-           // show a native JavaScript alert
-           alert("Hello World");
+
+        onAfterRendering() {
+            
+        },
+        onLoginAuth: function (oEvent) {
+           var sLogin = this.getView().byId('idLoginInput').getValue(),
+            sPassword = this.getView().byId('idPasswordInput').getValue();
+
+            if (sLogin === 'serwis@hicron.com' && sPassword === 'Hicron11')
+            {
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("Launchpad");
+            }
         }
-     });
- });
+    });
+});
