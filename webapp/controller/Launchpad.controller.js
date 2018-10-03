e8033ad64c65c289c762920db650f2dcaf99e7a6
mjk <mikolaj.jaks@hicron.com>
2018-10-03 15:58:28 +0200

    Interal

--- webapp/controller/Launchpad.controller.js
+++ webapp/controller/Launchpad.controller.js
@@ -3,35 +3,53 @@ sap.ui.define([
 ], function (Controller) {
     "use strict";
     return Controller.extend("app.webapp.controller.Launchpad", {
-        globalData: null,
+        globalData: null, //1000 for every 10 sec.
         onInit: function () {
             this.globalData = sap.ui.getCore().getModel('data');
             this.getView().setModel(this.globalData);
         },
-        onAfterRendering: function () {
+        onAfterRendering: function (oEvent) {
+            this.reloadData();
+            var that = this;
+            // this.intervalHandle = setInterval(function(self) { 
+            //     sap.ui.getCore().getModel('data').refresh();
+            //     self.reloadData();
+            //  },  10000);
+             this.intervalID = setInterval(
+                (function(self) {         //Self-executing func which takes 'this' as self
+                    return function() {   //Return a function in the context of 'self'
+                        self.reloadData(); //Thing you wanted to run as non-window 'this'
+                    }
+                })(this),
+                10000     //normal interval, 'this' scope not impacted here.
+            ); 
+        },
+        reloadData: function(){
             var oLivingroomTile = this.getView().byId('idLivingroomTile'),
                 lightList = this.globalData.getProperty("/lights"),
-             getInfoStatus = function(status)
+            getInfoStatus = function(status)
             {
                 if (status === "1") return "Success"
                 else if (status === "0") return "Error"
                 else return "Warning"
+            },
+            getStatusName = function(status)
+            {
+                if (status === "1") return "ON"
+                else if (status === "0") return "OFF"
+                else return "ERROR"
             };
             $.each(lightList, function (index, value) {
                 if (lightList[index].name === "livingroom") {
                    var lightName = lightList[index].name;
-                   var lightStatus = lightList[0].status;
+                   var lightStatus = lightList[0].state;
 
                    oLivingroomTile.setTitle(lightName);
                    oLivingroomTile.setInfoState(getInfoStatus(lightStatus));
+                   oLivingroomTile.setInfo(getStatusName(lightStatus));
                 }
             })
         },
-        // getInfoStatus: function(status){
-        //     if (status === "1") return "Success"
-        //     else if (status === "0") return "Error"
-        //     else return "Warning"
-        // },
         reloadRepo: function() {
             $.ajax({
                 type: "POST",
