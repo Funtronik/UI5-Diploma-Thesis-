ae1cda1ee7c781d6fe22afb894333426409d15cc
mjk <mikolaj.jaks@hicron.com>
2018-11-27 14:36:54 +0100

    Scripts from Raspberry
    Just in case

--- scripts/lolo.py
+++ scripts/lolo.py
@@ -26,4 +26,62 @@ if os.path.isdir(ui5folder):
     
     git.Git("/var/www/html").clone(repoPath)
     unpackFiles()
-    print("Deleted and Imported")
+    print("Deleted and Imported");
+else:
+    deleteFirst()
+    git.Git("/var/www/html").clone(repoPath)
+    unpackFiles()
+    print("Just Imported");
+
+
+# //////////////////////////////////////////////////////////////////////
+
+import mysql.connector
+import paho.mqtt.client as mqtt
+import json
+import datetime
+
+now = datetime.datetime.now()
+ 
+MQTT_SERVER = "localhost"
+MQTT_PATH = "MyHome/RoomTemperatureA"
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="luigi",
+  passwd="raspberry",
+  database="MyHome"
+)
+ 
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    client.subscribe(MQTT_PATH)
+    client.publish("MyHome/RoomTemperatureQ")
+ 
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))    
+    j = json.loads(msg.payload)
+
+    date = now.strftime("%Y.%m.%d")
+    if len(str(now.minute)) == 1:
+        time = str(now.hour) + ":0" + str(now.minute) + ":00"
+    else:
+        time = str(now.hour) + ":" + str(now.minute) + ":00"
+    
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO roomtemperatures (date, time, temp, hum) VALUES (%s, %s, %s, %s)"
+    val = (date, time, j['Temp'], j['Hum'])
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
+    quit()
+ 
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+ 
+client.connect(MQTT_SERVER, 1883, 60)
+client.loop_forever()
